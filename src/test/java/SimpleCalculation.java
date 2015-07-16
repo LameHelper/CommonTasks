577b091e6dc77cf378b55d398744920a768b8697
daniel <daniel.mechkarski@gmail.com>
2015-07-16 19:30:19 +0300

    New common project

---
+++ src/test/java/SimpleCalculation.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class SimpleCalculation {
+    @Test
+    public void CalculateTwoDigits()
+    {
+        int c = 5;
+        int a = 5;
+        int b = 10;
+        int sum = a+ b;
+        System.out.println("sum = " + sum);
+
+    }
+}
